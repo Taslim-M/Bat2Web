0c8ccefeef11ac682eb8a86922d377deac96ac12
Taslim-M <taslimmahbub@hotmail.com>
2021-03-07 21:40:24 +0400

    Update app.js

--- Application_Server/app.js
+++ Application_Server/app.js
@@ -1,10 +1,10 @@
-var express=require("express"),
-  app=express(),
-  bodyParser=require("body-parser"),
-  mongoose=require("mongoose"),
-  querystring=require("querystring"),
-  Incident=require("./models/incidents"),
-  seedDb=require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  querystring = require("querystring"),
+  Incident = require("./models/incidents"),
+  seedDb = require("./seeds");
 
 mongoose.connect("mongodb://localhost:27017/bat_project", {
   useNewUrlParser: true,
@@ -13,62 +13,52 @@ mongoose.connect("mongodb://localhost:27017/bat_project", {
 
 seedDb();
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
 
 app.get("/", async function (req, res) {
-  const incidents=await Incident.find({}).sort({ time: "desc" }).limit(30);
+  const incidents = await Incident.find({}).sort({ time: "desc" }).limit(30);
   res.render("index", { incidents: incidents });
 });
 
 app.get("/filter", async function (req, res) {
-  let sp_name=req.query.species_name;
-  let from_date_str=req.query.from_date;
-  let to_date_str=req.query.to_date;
-  let num_detections=
-    req.query.num_detections!=""? parseInt(req.query.num_detections):15;
+  let sp_name = req.query.species_name;
+  let from_date_str = req.query.from_date;
+  let to_date_str = req.query.to_date;
+  let num_detections =
+    req.query.num_detections != "" ? parseInt(req.query.num_detections) : 15;
 
-  var from_date=new Date(2020, 02, 23); //oldest possible date
-  var to_date=Date.now();
+  var from_date = new Date(2020, 02, 23); //oldest possible date
+  var to_date = new Date(Date.now());
   //replace dates if given
-  if (from_date_str) from_date=new Date(from_date_str);
-  if (to_date_str) to_date=new Date(to_date_str);
+  if (from_date_str) from_date = new Date(from_date_str);
+  if (to_date_str) to_date = new Date(to_date_str);
 
   console.log(sp_name);
   console.log(from_date);
   console.log(to_date);
 
-  if (sp_name=="All") {
-    const incidents=await Incident.find({
-      time: {
-        $gte: from_date,
-        $lt: to_date,
-      },
-    })
-      .sort({ time: "desc" })
-      .limit(num_detections);
-    res.render("index", { incidents: incidents });
-  } else {
-    const incidents=await Incident.find({
-      bat_species: sp_name,
-      time: {
-        $gte: from_date,
-        $lt: to_date,
-      },
-    })
-      .sort({ time: "desc" })
-      .limit(num_detections);
-    msg="Showing "+incidents.length+" results for "+sp_name+" from "+from_date.toDateString()+" to "+to_date.toDateString();
-    res.render("index", { incidents: incidents, msg: msg });
 
-  });
+  const incidents = await Incident.find({
+    bat_species: sp_name,
+    time: {
+      $gte: from_date,
+      $lt: to_date,
+    },
+  })
+    .sort({ time: "desc" })
+    .limit(num_detections);
+  msg = "Showing " + incidents.length + " results for " + sp_name + " from " + from_date.toDateString() + " to " + to_date.toDateString();
+  res.render("index", { incidents: incidents, msg: msg });
+
+});
 app.get("/about-us", async function (req, res) {
   res.render("about-us");
 });
 app.get("/dashboard", async function (req, res) {
-  let pie_counts=await Incident.aggregate([
+  let pie_counts = await Incident.aggregate([
     {
       $group: {
         // Each `_id` must be unique, so if there are multiple
@@ -96,6 +86,6 @@ app.use(function (err, req, res, next) {
   res.send("500 - Server Error");
 });
 //start server
-app.listen(process.env.PORT||3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server Started");
 });
